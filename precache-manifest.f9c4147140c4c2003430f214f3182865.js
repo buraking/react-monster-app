451e9b637a732ac187f1e99e623541fffadac6a9
buraking <soer109a@stud.kea.dk>
2019-10-18 21:44:47 +0200

    Updates

---
+++ precache-manifest.f9c4147140c4c2003430f214f3182865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200fcfe2763416e30b82d61880d64a8d",
+    "url": "/react-monster-app/index.html"
+  },
+  {
+    "revision": "50a3ad9f51ec60b4dd30",
+    "url": "/react-monster-app/static/css/main.5efa3013.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/react-monster-app/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "50a3ad9f51ec60b4dd30",
+    "url": "/react-monster-app/static/js/main.c0856ae0.chunk.js"
+  },
+  {
+    "revision": "78899204469028fca62a",
+    "url": "/react-monster-app/static/js/runtime-main.a116e4ef.js"
+  }
+]);
